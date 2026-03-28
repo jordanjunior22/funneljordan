539d4fb7928cbe0998c1f1bf05b15a29c34531b0
jordanjunior22 <jordan.kheambo@final.edu.tr>
2026-03-28 20:41:16 +0100

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const pixelId = "801149143038789";
 
 export const metadata: Metadata = {
   title: "Les Solutions de Jordan le Développeur",
@@ -27,7 +30,38 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        {/* ✅ Facebook Pixel */}
+        <Script id="fb-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '${pixelId}');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        {/* Optional fallback (recommended) */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${pixelId}&ev=PageView&noscript=1`}
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react';
 import Head from 'next/head';
-
+import Script from "next/script";
 const pixelId = '801149143038789';
 const whatsappLink = "https://wa.me/237676828121?text=Bonjour Jordan, je souhaite lancer ma boutique avec ta solution";
 declare global {
@@ -22,18 +22,6 @@ export default function LandingPage() {
       <Head>
         <title>Les Solutions de Jordan le Développeur | E-commerce Pro au Cameroun</title>
         <meta name="description" content="Payez une seule fois, vendez à vie. Intégration MoMo & Orange Money incluse par Jordan le Développeur." />
-        <script dangerouslySetInnerHTML={{
-          __html: `
-          !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${pixelId}');
-          fbq('track', 'PageView');
-        `}} />
       </Head>
 
       {/* AMBIENT BACKGROUND */}
